12c744fb0bf4acd98450da6bdc7f4f873129b5e1
Nir Tamir <nirtamir2@gmail.com>
2017-10-15 16:29:03 +0300

    deploy to gh pages

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,7 @@
     <v-parallax src="./static/stars.jpg" height="600">
       <v-layout column align-center justify-center class="white--text">
         <v-avatar size="125px">
-          <img src="/static/profileImage.jpg" alt="Nir Tamir">
+          <img src="static/profileImage.jpg" alt="Nir Tamir">
         </v-avatar>
         <h1 class="white--text mb-2 display-3">Nir Tamir</h1>
         <div class="headline mb-3 text-xs-center">Software Developer</div>
